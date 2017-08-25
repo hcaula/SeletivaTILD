2fcd1b31f3eeaf51da0bb3cbc74a7c66b3a59ae0
hcaula <henrique.caula@hotmail.com>
2017-08-25 14:24:22 -0300

    Resolve resizing windows

--- js/animations.js
+++ js/animations.js
@@ -3,6 +3,8 @@ var logo = $(".logo");
 var division = $(".division");
 var login_box = $(".login-box");
 
+document.getElementById("division").innerHTML = "";
+
 if(window.innerWidth < 906) division.hide();
 else division.show();
 
@@ -10,11 +12,14 @@ logo.hide();
 login_box.css("opacity", 0.0);
 
 body.onload = function(){
-  logo.fadeIn(500, function(){
+  logo.fadeIn(1000, function(){
     login_box.animate({
       opacity: 1
-    }, 500);
+    }, 1000);
   });
+  division.animate({
+    height: "50%"
+  }, 2000);
 }
 
 $( window ).resize(function() {
