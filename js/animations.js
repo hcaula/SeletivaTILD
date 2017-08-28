5da519c42fe117105c9fe3e3b5de037680e785ad
hcaula <henrique.caula@hotmail.com>
2017-08-28 11:12:25 -0300

    Add initialize function

--- js/animations.js
+++ js/animations.js
@@ -1,33 +1,43 @@
 /* Setting variables for easier reading later. */
-var body = document.getElementById("body");
-var logo = $(".logo");
-var division = $(".division");
-var login_box = $(".login-box");
+var body;
+var logo;
+var division;
+var login_box;
 
 
-/*
- * Set inner HTML of the division line to none
- * just so there is some text on the HTML source.
-*/
-// document.getElementById("division").innerHTML = "";
+var initialize = function(page){
+  if (page == 'login') initializeLoginPage();
+}
 
+var initializeLoginPage = function(){
+  body = document.getElementById("body");
+  logo = $(".logo");
+  division = $(".division");
+  login_box = $(".login-box");
 
-/* If the window is small, don't display the division line. */
-if(window.innerWidth < 906) division.hide();
-else division.show();
+  /*
+   * Set inner HTML of the division line to none
+   * just so there is some text on the HTML source.
+  */
+  document.getElementById("division").innerHTML = "";
 
-/* Initially hiding elements for animation. */
-logo.hide();
-login_box.hide();
+  /* If the window is small, don't display the division line. */
+  if(window.innerWidth < 906) division.hide();
+  else division.show();
 
-/* As the background image loads, animate. */
-// body.onload = function(){
-//   logo.fadeIn(1000, function(){});
-//   login_box.toggle(1000);
-//   division.animate({
-//     height: "50%"
-//   }, 1000);
-// };
+  /* Initially hiding elements for animation. */
+  logo.hide();
+  login_box.hide();
+
+  /* As the background image loads, animate. */
+  body.onload = function(){
+    logo.fadeIn(1000, function(){});
+    login_box.toggle(1000);
+    division.animate({
+      height: "50%"
+    }, 1000);
+  };
+}
 
 /* When clicked on input, make it the only window */
 var focused = false;
